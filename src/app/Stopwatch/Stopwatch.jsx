60badef2446cb682fa7d595be13f86a3a06874b5
Amith Francis <agusamith@gmail.com>
2020-05-09 15:43:14 +0530

    app - stopwatch coundown formatting , touch control

--- src/app/Stopwatch/Stopwatch.jsx
+++ src/app/Stopwatch/Stopwatch.jsx
@@ -6,6 +6,11 @@ import pauseImage from './pause.svg'
 
 class Stopwatch extends Component {
 
+    stopwatchElm;
+    countdownElm;
+
+    touchMoved=null;
+
     constructor(props){
         super(props)
         this.state = {
@@ -82,11 +87,29 @@ class Stopwatch extends Component {
         return new Date(ms).toISOString().slice(11, -2);
     }
 
+    msToFormattedTime(ms) {
+        let time = new Date(ms).toISOString();
+        if (ms < 60000) {          
+            time = ms < 10000? time.slice(18, -2) : time.slice(17, -2);
+        } else if (ms > 60000 && ms < 3600000) {
+            time = ms < 600000? time.slice(15,-2) : time.slice(14, -2);
+        } else {
+            time = time.slice(11, -2);
+        }
+        return time;
+    }
+
     componentDidMount() {
+        this.stopwatchElm = document.getElementById('stopwatch')
         window.addEventListener('keydown', this.handleKeydown.bind(this));
+        this.stopwatchElm.addEventListener('click', this.handleClick.bind(this));
+        this.countdownElm = document.getElementById('countdown');
+        this.countdownElm.addEventListener('touchstart',this.handleTouch.bind(this),true)
     }
     componentWillUnmount() {
         window.removeEventListener('keydown', this.handleKeydown.bind(this));
+        this.stopwatchElm.removeEventListener('click', this.handleClick.bind(this));
+        this.countdownElm.removeEventListener('touchstart',this.handleTouch.bind(this),true)
     }
 
     handleKeydown(e) {
@@ -104,17 +127,37 @@ class Stopwatch extends Component {
         }
     }
 
+    handleClick(){
+        this.toggle()
+    }
+
+    handleTouch(e){
+        this.countdownElm.addEventListener('touchmove',(e) => {
+            console.log('touchmove',e)
+            this.touchMoved = true;
+        },true);
+        // this.countdownElm.addEventListener('touchcancel',(e) => {
+        //     console.log('touchcancel',e)
+        // },true);
+        this.countdownElm.addEventListener('touchend', (e) => {
+            if (this.touchMoved) {
+                this.reset()
+                this.touchMoved = null;
+            }
+        },true);
+    }
+
     render(){    
-        return <div className="d-flex align-items-center justify-content-center h-100">
+        return <div id="stopwatch" className="d-flex align-items-center justify-content-center h-100">
             <div>
-                <div className="countdown">{this.msToTime(this.state.elapsedMS)}</div>
-                <div className="d-flex justify-content-center actions">
+                <div id="countdown" className="countdown">{this.msToFormattedTime(this.state.elapsedMS)}</div>
+                {/* <div className="d-flex justify-content-center actions">
                     <button className="toggle-btn mr-35" onClick={this.toggle}>
                         { this.state.status === 'ticking'? 
                             <img src={pauseImage}/> : <img src={playImage}/> }
                     </button>
                     <button className="reset-btn" onClick={this.reset} ><img src={resetImage} /></button>
-                </div>
+                </div> */}
             </div>
         </div>
     }
